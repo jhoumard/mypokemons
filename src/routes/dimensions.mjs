233fcc4191dc48e5c2945f561379a46f16bee5d2
jhoumard <jonathan.houmard@eduvaud.ch>
2024-12-28 10:14:43 +0100

    add get route to get pokemons of 1 specific dimension

--- src/routes/dimensions.mjs
+++ src/routes/dimensions.mjs
@@ -1,5 +1,6 @@
 import express from "express";
-import { dimensions } from "../db/mock-dimension.mjs";
+import { dimensions, getDimension } from "../db/mock-dimension.mjs";
+import { pokemons } from "../db/mock-pokemon.mjs";
 
 const dimensionsRouter = express();
 
@@ -10,9 +11,17 @@ dimensionsRouter.get("/", (req, res) => {
 
 dimensionsRouter.get("/:id", (req, res) => {
     const id = req.params.id;
-    const dim = dimensions.find(x => x.id == id);
+    const dim = getDimension(id);
     const message = `La dimension dont l'id vaut ${id} a bien été récupérée.`;
     res.json({ message, data: dim });
+});
+
+dimensionsRouter.get("/:id/pokemons", (req, res) => {
+    const dim_id = req.params.id;
+    const dim = getDimension(dim_id);
+    const dim_pokemons = pokemons.filter(x => x.dimensionId == dim_id);
+    const message = `La liste des pokemons à dimension #${dim_id} (${dim.name}) a bien été récupérée!`;
+    res.json({ message, data: dim_pokemons });
 });
 
 export { dimensionsRouter };
