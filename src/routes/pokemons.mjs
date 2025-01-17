23e95abbd73cf972df338d96c0d48891944208e3
Adrien Volery <pd52nbo@eduvaud.ch>
2025-01-17 13:52:50 +0100

    Route pour pokemon Particulier

--- src/routes/pokemons.mjs
+++ src/routes/pokemons.mjs
@@ -7,14 +7,14 @@ const pokemonsRouter = express();
 // Obtenir la liste des pokemons.
 pokemonsRouter.get("/", (req, res) => {
     Pokemon.findAll()
-    .then(pokemons => {
-        const message = "La liste des pokemons a bien été récupérée !";
-        res.json(success(message, pokemons));
-    })
-    .catch(error => {
-        const message = "Erreur 500: La liste des pokemons n'a pas pu être récupérée. Merci de réessayer plus tard.";
-        res.status(500).json({ message, data: error });
-    });
+        .then(pokemons => {
+            const message = "La liste des pokemons a bien été récupérée !";
+            res.json(success(message, pokemons));
+        })
+        .catch(error => {
+            const message = "Erreur 500: La liste des pokemons n'a pas pu être récupérée. Merci de réessayer plus tard.";
+            res.status(500).json({ message, data: error });
+        });
 });
 
 // Obtenir un pokemon en particulier.
