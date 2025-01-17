252a296b5121a15028e7b9b5ed87547fac7e13e3
Anthony Vuagniaux <pb25tik@eduvaud.ch>
2025-01-17 13:55:45 +0100

    commit  route obtenir la liste et obtenir un pokemon routes/pokemons.mjs

--- src/routes/pokemons.mjs
+++ src/routes/pokemons.mjs
@@ -23,13 +23,30 @@ pokemonsRouter.get("/", (req, res) => {
 
 // Obtenir un pokemon en particulier.
 pokemonsRouter.get("/:id", (req, res) => {
-
+    Pokemon.findByPk(req.params.id)
+        .then(pokemon => {
+            if (pokemon === null) {
+                const message = "Erreur 404: Le pokemon demandé n'existe pas. Merci de réessayer avec un autre identifiant.";
+                return res.status(404).json({ message });
+            }
+            const message = `Le pokemon dont l'id vaut ${pokemon.id} a bien été récupéré.`;
+            res.json(success(message, pokemon));
+        })
 });
 
 // Ajouter un pokemon.
 pokemonsRouter.post("/", (req, res) => {
-
+    Pokemon.create(req.body)
+        .then(createdPokemon => {
+            const message = `Le pokemon ${createdPokemon.name} a bien été ajouté !`;
+            res.json(success(message, createdPokemon));
+        })
+        .catch(error => {
+            const message = "Erreur 500: Le pokemon n'a pas pu être ajouté. Merci de réessayer plus tard.";
+            res.status(500).json({ message, data: error });
+        });
 });
+
 
 // Modifier un pokemon.
 pokemonsRouter.put("/:id", (req, res) => {
