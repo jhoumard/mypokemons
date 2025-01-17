52b80c76008a99b1f9ac022cd60606588e7c87e6
Amael Jampen <pw35rtf@eduvaud.ch>
2025-01-17 14:14:26 +0100

    feat: Gestion des règles de validation de pokemons

--- src/models/pokemons.mjs
+++ src/models/pokemons.mjs
@@ -18,9 +18,42 @@ const PokemonModel = (sequelize, DataTypes) => {
             name: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                unique: {
+                    msg: "Ce nom est deja pris.",
+                },
+                validate: {
+                    is: {
+                        args: /^[A-Za-z]*$/,
+                        msg: "Seules les lettres sont autorisées.",
+                    },
+
+                    notEmpty: {
+                        msg: "Le nom ne peut pas être vide.",
+                    },
+                    notNull: {
+                        msg: "Le nom est une propriété obligatoire.",
+                    },
+                }
             },
             dimensionId: {
                 type: DataTypes.INTEGER,
+                validate: {
+
+                    notEmpty: {
+                        msg: "Le prix ne peut pas être vide.",
+                    },
+                    notNull: {
+                        msg: "Le prix est une propriété obligatoire.",
+                    },
+                    min: {
+                        args: [1.0],
+                        msg: "Le prix doit être supérieur ou égal à 1.",
+                    },
+                    max: {
+                        args: [3.0],
+                        msg: "Le prix doit être inférieur ou égal à 3.",
+                    },
+                },
             },
         },
         {
