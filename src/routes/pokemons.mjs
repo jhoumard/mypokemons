d250ad821038d6a40ab09883fcfdd74383fb82ec
Alonzo Pinto Dos Santos Gaspar <pe48qqd@eduvaud.ch>
2025-01-17 14:09:10 +0100

    ex2

--- src/routes/pokemons.mjs
+++ src/routes/pokemons.mjs
@@ -29,7 +29,7 @@ pokemonsRouter.get("/:id", (req, res) => {
     const id = req.params.id;
     Pokemon.findByPk(id)
         .then(pokemon => {
-            const message = `Le pokemon dont l'id vaut ${id} a bien été trouvé.`;
+            const message = `Le pokemon dont l'id vaut ${pokemon.id} a bien été trouvé.`;
             res.json(success(message, pokemon));
         })
         .catch(error => {
