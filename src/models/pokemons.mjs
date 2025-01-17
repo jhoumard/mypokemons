57c197e4e4a99528b718149d850edc3ced57d1a2
Jérémie Noah Chevalley <pb83pyi@eduvaud.ch>
2025-01-17 14:21:21 +0100

    Exercice 4

--- src/models/pokemons.mjs
+++ src/models/pokemons.mjs
@@ -1,4 +1,8 @@
-// https://sequelize.org/docs/v7/models/data-types/
+/*
+Author : Jérémie Chevalley
+Date : 17.01.2025
+Description : DB model for the pokemons
+*/
 
 const PokemonModel = (sequelize, DataTypes) => {
     return sequelize.define(
@@ -12,15 +16,45 @@ const PokemonModel = (sequelize, DataTypes) => {
             name: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                unique: { msg: "Ce nom de Pokemon est déjà pris" },
+                validate: {
+                    is: {
+                        args: /^[A-Za-z\séè]*$/,
+                        msg: "Seules les lettres, les espaces et certains caractères spéciaux (é,è) sont autorisées."
+                    },
+                    notEmpty: {
+                        msg: "Le nom ne peut pas être vide.",
+                    },
+                    notNull: {
+                        msg: "Le nom est une propriété obligatoire."
+                    }
+                }
             },
             dimensionId: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: "Le nom ne peut pas être vide.",
+                    },
+                    notNull: {
+                        msg: "Le nom est une propriété obligatoire."
+                    },
+                    min: {
+                        args: [1.0],
+                        msg: "Les dimensions valides sont 1,2 et 3"
+                    },
+                    max: {
+                        args: [3.0],
+                        msg: "Les dimensions valides sont 1,2 et 3"
+                    }
+                }
             },
         },
         {
             timestamps: true,
             createdAt: "created",
-            updatedAt: false,
+            updatedAt: "updated",
         }
     );
 };
