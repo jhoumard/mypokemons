b641a190fad48ef0c55007a746844e64bcb2caa0
Anthony Vuagniaux <pb25tik@eduvaud.ch>
2025-01-17 14:14:18 +0100

    commit ajouts des paramètres demandé  models/pokemons.mjs

--- src/models/pokemons.mjs
+++ src/models/pokemons.mjs
@@ -1,3 +1,6 @@
+// Anthony Vuagniaux
+// Date 17.01.25
+
 // https://sequelize.org/docs/v7/models/data-types/
 
 const PokemonModel = (sequelize, DataTypes) => {
@@ -12,9 +15,32 @@ const PokemonModel = (sequelize, DataTypes) => {
             name: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                validate: {
+                    is: {
+                        args: /^[A-Za-z\s]*$/,
+                        msg: "Seules les lettres",
+                    }
+                },
+                unique:{
+                    msg:"Ce nom est déjà pris"
+                },
+                notEmpty: {
+                    msg: "Le nom ne peut pas être vide."
+                },
+                notNull: {
+                    msg: "Le nom est obligatoire."
+                }
+
+
             },
             dimensionId: {
                 type: DataTypes.INTEGER,
+                notEmpty: {
+                    msg: "La dimmension ne peut pas être vide"
+                },
+                notNull: {
+                    msg: "La dimmension est obligatoire"
+                }
             },
         },
         {
