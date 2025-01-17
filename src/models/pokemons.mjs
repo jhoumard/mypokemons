81715aff19ae55c2c67db26384294891999f1632
Chris Brandt <pl52dsy@eduvaud.ch>
2025-01-17 14:31:43 +0100

    règles de validations

--- src/models/pokemons.mjs
+++ src/models/pokemons.mjs
@@ -1,4 +1,8 @@
 // https://sequelize.org/docs/v7/models/data-types/
+/*
+Chris Brandt
+17.01.25
+*/
 
 const PokemonModel = (sequelize, DataTypes) => {
     return sequelize.define(
@@ -12,9 +16,44 @@ const PokemonModel = (sequelize, DataTypes) => {
             name: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                unique: {
+                    msg: "Ce nom est déjà pris.",
+                },
+                validate: {
+                    is: {
+                        args: /[A-Za-z\s]*/,
+                        msg: "Seules les lettres sont autorisées.",
+                    },
+                    notEmpty: {
+                        msg: "Le nom ne peut pas être vide.",
+                    },
+                    notNull: {
+                        msg: "Le nom est une propriété obligatoire.",
+                    },
+                },
             },
             dimensionId: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: "La dimension ne peut pas être vide.",
+                    },
+                    notNull: {
+                        msg: "La dimension est une propriété obligatoire.",
+                    },
+                    isInt: {
+                        msg: "Utilisez uniquement des nombre de 1 à 3.",
+                    },
+                    min: {
+                        args: [1],
+                        msg: "La dimension doit être supérieur à 1.",
+                    },
+                    max: {
+                        args: [3],
+                        msg: "La dimension doit être inférieur à 3.",
+                    },
+                },
             },
         },
         {
