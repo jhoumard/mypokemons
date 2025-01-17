ee681c78d08d30a57ba5588c1623d1dbd64c2633
Samuel Gergely <pu22amq@eduvaud.ch>
2025-01-17 14:15:35 +0100

    feat : add validation to pokemons models

--- src/models/pokemons.mjs
+++ src/models/pokemons.mjs
@@ -1,3 +1,8 @@
+/*
+* Auteur : Samuel Gergely
+* Date : 17.01.2025
+* */
+
 // https://sequelize.org/docs/v7/models/data-types/
 
 const PokemonModel = (sequelize, DataTypes) => {
@@ -12,9 +17,34 @@ const PokemonModel = (sequelize, DataTypes) => {
             name: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                unique: {
+                    msg: "Ce nom est déjà pris.",
+                },
+                validate: {
+                    is: {
+                        args: /^[A-Za-z\séè]*$/,
+                    },
+                },
             },
             dimensionId: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: "La dimension doit être renseigné",
+                    },
+                    notNull: {
+                        msg: "La dimension est une propriété obligatoire.",
+                    },
+                    min: {
+                        args: [1.0],
+                        msg: "Le numéros de la dimension doit etre superieur ou égale à 1.",
+                    },
+                    max: {
+                        args: [3.0],
+                        msg: "Le numéros de la dimension doit être inférieur ou égale à 3.",
+                    },
+                },
             },
         },
         {
