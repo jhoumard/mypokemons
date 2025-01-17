25da3febd2e3670e1f1c0c96cde3a50cdc08a626
Marco Mascellino <pp47nln@eduvaud.ch>
2025-01-17 13:51:17 +0100

    etape 1

--- src/routes/pokemons.mjs
+++ src/routes/pokemons.mjs
@@ -1,3 +1,9 @@
+/*
+Author: Marco Mascellino
+Date: 17.01.2025
+*/
+
+
 import express from "express";
 import { Pokemon } from "../db/sequelize.mjs";
 import { success } from "./helpers.mjs";
@@ -19,22 +25,33 @@ pokemonsRouter.get("/", (req, res) => {
 
 // Obtenir un pokemon en particulier.
 pokemonsRouter.get("/:id", (req, res) => {
-
+    const id = req.params.id;
+    Pokemon.findByPk(id)
+        .then(pokemon => {
+            if (pokemon) {
+                const message = "Le pokemon a bien été récupéré !";
+                res.json(success(message, pokemon));
+            } else {
+                const message = "Erreur 404: Le pokemon n'a pas été trouvé.";
+                res.status(404).json({ message });
+            }
+        })
+        .catch(error => {
+            const message = "Erreur 500: Le pokemon n'a pas pu être récupéré. Merci de réessayer plus tard.";
+            res.status(500).json({ message, data: error });
+        });
 });
 
 // Ajouter un pokemon.
 pokemonsRouter.post("/", (req, res) => {
-
 });
 
 // Modifier un pokemon.
 pokemonsRouter.put("/:id", (req, res) => {
-
 });
 
 // Supprimer un pokemon.
 pokemonsRouter.delete("/:id", (req, res) => {
-
 });
 
 export { pokemonsRouter };
