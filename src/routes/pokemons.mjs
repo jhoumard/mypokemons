a165fadee12dc5d997eefd2980968cc956aea4e2
vikki6408 <vikki.dolt@eduvaud.ch>
2025-01-17 14:03:48 +0100

    ajouter pokemon

--- src/routes/pokemons.mjs
+++ src/routes/pokemons.mjs
@@ -19,22 +19,55 @@ pokemonsRouter.get("/", (req, res) => {
 
 // Obtenir un pokemon en particulier.
 pokemonsRouter.get("/:id", (req, res) => {
-
+    Pokemon.findByPk(req.params.id).then((pokemons) => {
+        if (pokemons === null) {
+            const message =
+                "Le pokemon demandé n'existe pas. Merci de réessayer avec un autre identifiant.";
+            // A noter ici le return pour interrompre l'exécution du code
+            return res.status(404).json({ message });
+        }
+      
+        const message = `Le pokemon dont l'id vaut ${product.id} a bien été récupéré.`;
+        res.json(success(message, pokemons));
+    })
+    .catch((error) => {
+        const message =
+            "Le pokemon n'a pas pu être récupéré. Merci de réessayer dans quelques instants.";
+        res.status(500).json({ message, data: error });
+    });
 });
 
 // Ajouter un pokemon.
 pokemonsRouter.post("/", (req, res) => {
+    Pokemon.create(req.body).then((createdPokemon) => {
+        // Définir un message pour le consommateur de l'API REST
+        const message = `Le pokemon ${createdPokemon.name} a bien été créé !`;
 
+        // Retourner la réponse HTTP en json avec le msg et le produit créé
+        res.json(success(message, createdPokemon));
+    })
+    .catch((error) => {
+        if (error instanceof ValidationError) {
+            return res.status(400).json({ message: error.message, data: error });
+        }
+        const message =
+            "Le pokemon n'a pas pu être ajouté. Merci de réessayer dans quelques instants.";
+        res.status(500).json({ message, data: error });
+    });
 });
 
 // Modifier un pokemon.
-pokemonsRouter.put("/:id", (req, res) => {
-
+pokemonsRouter.put("/:id", (res) => {
+    const message =
+            "La suppression du pokemon est refusée";
+        res.status(403).json({ message, data: error });
 });
 
 // Supprimer un pokemon.
 pokemonsRouter.delete("/:id", (req, res) => {
-
+    const message =
+    "La suppression du pokemon est refusée";
+res.status(403).json({ message, data: error });
 });
 
 export { pokemonsRouter };
