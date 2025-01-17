a831dbfac8489464f64ce90f327b3911944ba835
Hugo652-droid <hugo.rod@bluewin.ch>
2025-01-17 14:05:38 +0100

    validation de l'unicité et des charactaires du nom

--- src/models/pokemons.mjs
+++ src/models/pokemons.mjs
@@ -12,6 +12,17 @@ const PokemonModel = (sequelize, DataTypes) => {
             name: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                unique: {
+                    msg: "Ce nom est déjà pris."
+                },
+                validate: {
+                    is: {
+                        // contraint de charactére sont dans les []. A-Z veut dire de A majuscule à Z majuscule. a-z veut dire la meme chose mais minuscule
+                        // \s veut dire autorisé les espaces
+                        args: /^[A-Za-z\séè]*$/,
+                        msg: "Seules les lettres et les espaces sont autorisées.",
+                    },
+                }
             },
             dimensionId: {
                 type: DataTypes.INTEGER,
